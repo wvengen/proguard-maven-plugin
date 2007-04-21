4f58d3ae59bcba39c5a4f17b1fb4a45110708501
vlads <vlads@64363475-2d43-4506-900a-d995b6e1408b>
2007-04-21 01:33:52 +0000

    add header to all files
    git-svn-id: https://pyx4j.com/svn/pyx4me/pyx4me-maven-plugins/proguard-maven-plugin@103 64363475-2d43-4506-900a-d995b6e1408b

--- src/main/java/com/pyx4me/maven/proguard/Inclusion.java
+++ src/main/java/com/pyx4me/maven/proguard/Inclusion.java
@@ -1,5 +1,11 @@
+/**
+ * Pyx4me framework
+ * Copyright (C) 2006-2007 pyx4.com.
+ * 
+ * @author vlads
+ * @version $Id$
+ */
 package com.pyx4me.maven.proguard;
-
 
 public class Inclusion extends ArtifactFilter {
 
--- src/main/java/com/pyx4me/maven/proguard/ProGuardMojo.java
+++ src/main/java/com/pyx4me/maven/proguard/ProGuardMojo.java
@@ -1,3 +1,10 @@
+/**
+ * Pyx4me framework
+ * Copyright (C) 2006-2007 pyx4.com.
+ * 
+ * @author vlads
+ * @version $Id$
+ */
 package com.pyx4me.maven.proguard;
 
 import java.io.File;
