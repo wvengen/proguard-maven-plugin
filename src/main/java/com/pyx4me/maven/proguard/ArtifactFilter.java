1041eb6cbc8181d3bc52c162d0ac9b477785764e
vlads <vlads@64363475-2d43-4506-900a-d995b6e1408b>
2008-06-21 23:16:18 +0000

    Updated headers: Licensed under the Apache License
    git-svn-id: https://pyx4j.com/svn/pyx4me/pyx4me-maven-plugins/proguard-maven-plugin@2066 64363475-2d43-4506-900a-d995b6e1408b

--- src/main/java/com/pyx4me/maven/proguard/ArtifactFilter.java
+++ src/main/java/com/pyx4me/maven/proguard/ArtifactFilter.java
@@ -1,6 +1,19 @@
 /**
  * Pyx4me framework
- * Copyright (C) 2006-2007 pyx4.com.
+ * Copyright (C) 2006-2008 pyx4j.com.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  * 
  * @author vlads
  * @version $Id$
@@ -12,14 +25,14 @@ import org.apache.maven.artifact.Artifact;
 public class ArtifactFilter {
 
 	protected String groupId;
-	
+
 	protected String artifactId;
-	
+
 	protected String classifier;
-	
+
 	public boolean match(Artifact artifact) {
-			return (artifact.getArtifactId().equals(this.artifactId) && artifact.getGroupId().equals(this.groupId)
-					&& (((this.classifier == null) && (artifact.getClassifier() == null)) || 
-							((this.classifier != null) && this.classifier.equals(artifact.getClassifier())))); 
+		return (artifact.getArtifactId().equals(this.artifactId) && artifact.getGroupId().equals(this.groupId) && (((this.classifier == null) && (artifact
+				.getClassifier() == null)) || ((this.classifier != null) && this.classifier.equals(artifact
+				.getClassifier()))));
 	}
 }
