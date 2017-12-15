10e134245825a25842e170ec12c08c99b86b261c
Liam Sharp <liam.sharp@gmail.com>
2017-12-15 07:36:43 +0000

    Add support for Java 9 multi release jars #61 - A property for library jar exclusions (libraryJarExclusion)

--- src/main/java/com/github/wvengen/maven/proguard/ProGuardMojo.java
+++ src/main/java/com/github/wvengen/maven/proguard/ProGuardMojo.java
@@ -124,6 +124,12 @@ public class ProGuardMojo extends AbstractMojo {
 	 */
 	private boolean putLibraryJarsInTempDir;
 
+	/**
+	 * Sets an exclude for all library jars, eg: (!META-INF/versions/**)
+	 *
+	 * @parameter default-value=""
+	 */
+	private String libraryJarExclusion;
 
 	/**
 	 * Specifies that project compile dependencies should be added as injar.
@@ -475,12 +481,7 @@ public class ProGuardMojo extends AbstractMojo {
 					// This may not be CompileArtifacts, maven 2.0.6 bug
 					File file = getClasspathElement(getDependency(inc, mavenProject), mavenProject);
 					inPath.add(file.toString());
-					if(putLibraryJarsInTempDir){
-						libraryJars.add(file);
-					} else {
-						args.add("-libraryjars");
-						args.add(fileToString(file));
-					}
+					addLibraryJar(args, libraryJars, file);
 				}
 			}
 		}
@@ -530,12 +531,7 @@ public class ProGuardMojo extends AbstractMojo {
 					args.add(fileToString(file));
 				} else {
 					log.debug("--- ADD libraryjars:" + artifact.getArtifactId());
-					if (putLibraryJarsInTempDir) {
-						libraryJars.add(file);
-					} else {
-						args.add("-libraryjars");
-						args.add(fileToString(file));
-					}
+					addLibraryJar(args, libraryJars, file);
 				}
 			}
 		}
@@ -565,12 +561,7 @@ public class ProGuardMojo extends AbstractMojo {
 
 		if (libs != null) {
 			for (String lib : libs) {
-				if (putLibraryJarsInTempDir) {
-					libraryJars.add(new File(lib));
-				} else {
-					args.add("-libraryjars");
-					args.add(fileNameToString(lib));
-				}
+				addLibraryJar(args, libraryJars, new File(lib));
 			}
 		}
 
@@ -687,6 +678,19 @@ public class ProGuardMojo extends AbstractMojo {
 			}
 			if (attachSeed) {
 				attachTextFile(new File(buildOutput, seedFileName), mainClassifier, "seed");
+			}
+		}
+	}
+
+	private void addLibraryJar(ArrayList<String> args, ArrayList<File> libraryJars, File file)
+	{
+		if (putLibraryJarsInTempDir) {
+			libraryJars.add(file);
+		} else {
+			args.add("-libraryjars");
+			args.add(fileToString(file));
+			if (libraryJarExclusion != null) {
+				args.add(libraryJarExclusion);
 			}
 		}
 	}
